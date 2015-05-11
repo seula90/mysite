bb564ec971e98065462d7d9cca73642a5d00dbea
박슬아 <seula9002@gmail.com>
2015-05-11 17:17:10 +0900

    prototype 추가

---
+++ src/com/sds/icto/mysite/servlet/action/guestbook/AddAction.java
@@ -0,0 +1,38 @@
+package com.sds.icto.mysite.servlet.action.guestbook;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import com.sds.icto.mysite.dao.GuestBookDao;
+import com.sds.icto.mysite.vo.GuestBookVo;
+
+import com.sds.icto.web.Action;
+
+public class AddAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ClassNotFoundException, ServletException,
+			IOException {
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+		String message = request.getParameter("message");
+		
+		GuestBookVo vo = new GuestBookVo();
+		vo.setName(name);
+		vo.setPwd(password);
+		vo.setMsg(message);
+		
+		
+		GuestBookDao dao = new GuestBookDao();
+		dao.insert(vo);
+		
+		response.sendRedirect("/mysite/gb");
+	}
+
+}
